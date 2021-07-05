1234f61ecdc9c7e6a91b8a3069e71fb8aafbbf3f
Gia Bảo <noffyns@outlook.com>
2021-07-05 14:47:57 +0700

    Create Course successed

---
+++ 1811063256_NguyenGiaBao_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using _1811063256_NguyenGiaBao_BigSchool.Models;
+using _1811063256_NguyenGiaBao_BigSchool.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1811063256_NguyenGiaBao_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
--- 1811063256_NguyenGiaBao_BigSchool/Models/ApplicationUser.cs
+++ 1811063256_NguyenGiaBao_BigSchool/Models/ApplicationUser.cs
@@ -17,17 +17,4 @@ namespace _1811063256_NguyenGiaBao_BigSchool.Models
             return userIdentity;
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
