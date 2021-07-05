028dfa882d01bbd2dffb9e4e926f6d080ca790ce
Gia Bảo <noffyns@outlook.com>
2021-07-05 15:01:55 +0700

    Required for Create Course

--- 1811063256_NguyenGiaBao_BigSchool/ViewModel/CourseViewModel.cs
+++ 1811063256_NguyenGiaBao_BigSchool/ViewModel/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using _1811063256_NguyenGiaBao_BigSchool.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace _1811063256_NguyenGiaBao_BigSchool.ViewModel
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
