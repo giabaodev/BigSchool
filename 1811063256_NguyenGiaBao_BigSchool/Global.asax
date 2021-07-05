2a0eacd59097df66eb2079ab3954fe6e29537642
Gia Bảo <noffyns@outlook.com>
2021-07-05 14:19:50 +0700

    Add project files.

---
+++ 1811063256_NguyenGiaBao_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811063256_NguyenGiaBao_BigSchool.MvcApplication" Language="C#" %>
---
+++ 1811063256_NguyenGiaBao_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811063256_NguyenGiaBao_BigSchool.Startup))]
+namespace _1811063256_NguyenGiaBao_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
