3d58f272b1151a2ff840d3e574db096b2ee04cf0
Gia Bảo <noffyns@outlook.com>
2021-07-05 16:23:06 +0700

    Page 36

--- 1811063256_NguyenGiaBao_BigSchool/Controllers/HomeController.cs
+++ 1811063256_NguyenGiaBao_BigSchool/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using _1811063256_NguyenGiaBao_BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace _1811063256_NguyenGiaBao_BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Lecturer)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
--- 1811063256_NguyenGiaBao_BigSchool/Models/Category.cs
+++ 1811063256_NguyenGiaBao_BigSchool/Models/Category.cs
@@ -11,6 +11,6 @@ namespace _1811063256_NguyenGiaBao_BigSchool.Models
         public byte Id { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+        public string CategoryName { get; set; }
     }
 }
